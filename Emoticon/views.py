64c03e6ddaae0e9369aa51153f28f25846cd4308
Akshay Verma <topensite@gmail.com>
2024-03-10 19:36:01 +0530

    image upload functionality added

--- Emoticon/views.py
+++ Emoticon/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts  import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from .modules.forms import UploadFileForm
+import os
+
+APP_PATH = os.getcwd()
 
 def home(request):
     return render(request,"index.html")
+
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(request.FILES['file'])
+            # Redirect to a success page or render a success message
+            return HttpResponseRedirect('/success/')
+    else:
+        form = UploadFileForm()
+    return render(request, 'upload_file.html', {'form': form})
+
+def handle_uploaded_file(file):
+    with open(APP_PATH+'/Emoticon/data/tmp/' + file.name, 'wb+') as destination:
+        for chunk in file.chunks():
+            destination.write(chunk)
+
+def success_view(request):
+    return render(request, 'success.html')
 
 
